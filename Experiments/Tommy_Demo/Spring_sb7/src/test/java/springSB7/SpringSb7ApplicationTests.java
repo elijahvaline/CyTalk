9859d015aab18d487635f4c5078840d54c2c1ebf
Tommy <tschmid@iastate.edu>
2020-08-31 12:22:16 -0500

    sprinboot

---
+++ Experiments/Tommy_Demo/Spring_sb7/src/test/java/springSB7/SpringSb7ApplicationTests.java
@@ -0,0 +1,13 @@
+package springSB7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSb7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

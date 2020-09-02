d07f6b7c5b3882d1aad5e502969636cef2ce32b7
BuildTools <unconfigured@null.spigotmc.org>
2020-09-01 22:45:24 -0500

    2nd Experiment 1.2

--- Experiments/Casey_Demo/app/src/main/java/com/example/hello_world/ScrollingActivity.java
+++ Experiments/Casey_Demo/app/src/main/java/com/example/hello_world/ScrollingActivity.java
@@ -26,17 +26,7 @@ public class ScrollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
-        toolBarLayout.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         view = this.getWindow().getDecorView();
     }
 

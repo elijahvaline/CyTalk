db0ca5d533c612bb52e8dd5f1fa51c0f36ecdc25
BuildTools <unconfigured@null.spigotmc.org>
2020-09-14 21:26:58 -0500

    Tested Volley with Postman

--- Experiments/Casey_Demo/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
+++ Experiments/Casey_Demo/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
@@ -3,6 +3,6 @@ package com.example.sumon.androidvolley.utils;
 public class Const {
 	public static final String URL_JSON_OBJECT = "https://api.androidhive.info/volley/person_object.json";
 	public static final String URL_JSON_ARRAY = "https://api.androidhive.info/volley/person_array.json";
-	public static final String URL_STRING_REQ = "https://api.androidhive.info/volley/string_response.html";
+	public static final String URL_STRING_REQ = "https://2db66630-0bd6-4ef6-ac3d-8170b4b8a10f.mock.pstmn.io/test";
 	public static final String URL_IMAGE = "https://api.androidhive.info/volley/volley-image.jpg";
 }

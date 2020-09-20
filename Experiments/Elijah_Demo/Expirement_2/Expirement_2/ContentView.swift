d83ec586b123532f90930a92eaf4798de14eab99
Eli <elijahvaline@gmail.com>
2020-09-19 19:22:42 -0500

    Added ios project

--- Experiments/Elijah_Demo/Expirement_2/Expirement_2/ContentView.swift
+++ Experiments/Elijah_Demo/Expirement_2/Expirement_2/ContentView.swift
@@ -8,118 +8,252 @@
 
 import SwiftUI
 
-struct CheckboxToggleStyle: ToggleStyle {
-    let scaleFactor:Float = 0.12
-    
-    
-    func makeBody(configuration: Configuration) -> some View {
-        
-        return Image(systemName: configuration.isOn ? "arrow.up.circle.fill" : "arrow.up.circle")
-            
-            .font(.system(size:60))
-            .onTapGesture { configuration.isOn.toggle() }
-    }
-}
-struct CheckboxToggleStyleDown: ToggleStyle {
-    let scaleFactor:Float = 0.12
-    
-    
-    func makeBody(configuration: Configuration) -> some View {
-        
-        return Image(systemName: configuration.isOn ? "arrow.down.circle.fill" : "arrow.down.circle")
-            
-            .font(.system(size:60))
-            .onTapGesture { configuration.isOn.toggle() }
-    }
-}
-
 struct ContentView: View {
-    var screenWidth  = UIScreen.main.bounds.width
-    var screenHeight = UIScreen.main.bounds.height
-    
-    @State var posts:[String] = ["one", "two", "three", "four", "five", "size", "seven", "eight", "nine", "ten"]
-    @State var upvote:Bool = false
-    @State var downvote:Bool = false
-    
     var body: some View {
         
-       let onUp = Binding<Bool>(get: { self.upvote }, set: { self.upvote = $0; self.downvote = false})
-              let onDown = Binding<Bool>(get: { self.downvote}, set: { self.upvote = false; self.downvote = $0})
-        
-        
-       return ZStack{
+        ZStack{
             
-//
+            LinearGradient(gradient: .init(colors: [Color("Color"),Color("Color1"),Color("Color2")]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             
-            ScrollView{
-                VStack(spacing: 15) {
-                    
-                    ForEach(posts, id: \.self) { posts in
-                        
-                       
-
-                            
-                            
-                            
-                            ZStack{
-                                
-                                
-                                Rectangle()
-                                    .frame(width: self.screenWidth, height: 200)
-                                    .foregroundColor(.white)
-                                    .shadow(radius: 5)
-                                    .edgesIgnoringSafeArea(.all)
-                                
-                                HStack(spacing: 50 ){
-
-                                    Toggle(isOn: onUp){
-                                        Text("")
-                                    }
-                                    .toggleStyle(CheckboxToggleStyle())
-                                    .foregroundColor(.green)
-                                    
-                                    Text(posts)
-                                    .font(.title)
-                                    .foregroundColor(.blue)
-
-                                    Toggle(isOn: onDown){
-                                        Text("")
-                                    }
-                                    .toggleStyle(CheckboxToggleStyleDown())
-                                    .foregroundColor(.blue)
-                                }
-                                
-                                
-                                
-                            }
-                            
-                            
-                            
-                        
-                        
-                        
-                        
-                    }
-                    
-                }
-                .edgesIgnoringSafeArea(.all)
+            if UIScreen.main.bounds.height > 800{
                 
-                
-                
-                
+                Home()
             }
-        }.edgesIgnoringSafeArea(.all)
-        
-        
-        
-        
-        
-        
+            else{
+                
+                ScrollView(.vertical, showsIndicators: false) {
+                    
+                    Home()
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home : View {
+    
+    @State var index = 0
+    
+    var body : some View{
+        
+        VStack{
+            
+            Image("logo")
+            .resizable()
+            .frame(width: 200, height: 150)
+            
+            HStack{
+                
+                Button(action: {
+                    
+                    withAnimation(.spring(response: 0.8, dampingFraction: 0.5, blendDuration: 0.5)){
+                        
+                       self.index = 0
+                    }
+                    
+                }) {
+                    
+                    Text("Existing")
+                        .foregroundColor(self.index == 0 ? .black : .white)
+                        .fontWeight(.bold)
+                        .padding(.vertical, 10)
+                        .frame(width: (UIScreen.main.bounds.width - 50) / 2)
+                    
+                }.background(self.index == 0 ? Color.white : Color.clear)
+                .clipShape(Capsule())
+                
+                Button(action: {
+                    
+                   withAnimation(.spring(response: 0.8, dampingFraction: 0.5, blendDuration: 0.5)){
+                       
+                      self.index = 1
+                   }
+                    
+                }) {
+                    
+                    Text("New")
+                        .foregroundColor(self.index == 1 ? .black : .white)
+                        .fontWeight(.bold)
+                        .padding(.vertical, 10)
+                        .frame(width: (UIScreen.main.bounds.width - 50) / 2)
+                    
+                }.background(self.index == 1 ? Color.white : Color.clear)
+                .clipShape(Capsule())
+                
+            }.background(Color.black.opacity(0.1))
+            .clipShape(Capsule())
+            .padding(.top, 25)
+            
+            if self.index == 0{
+                
+                Login()
+            }
+            else{
+                
+                SignUp()
+            }
+            
+            if self.index == 0{
+
+            }
+            
+            
+           
+            
+        }
+        .padding()
+    }
+}
+
+struct Login : View {
+    
+    @State var mail = ""
+    @State var pass = ""
+    
+    var body : some View{
+        
+        VStack{
+            
+            VStack{
+                
+                HStack(spacing: 15){
+                    
+                    Image(systemName: "person")
+                        .foregroundColor(.black)
+                    
+                    TextField("Enter Email Address", text: self.$mail)
+                    
+                }.padding(.vertical, 20)
+                
+                Divider()
+                
+                HStack(spacing: 15){
+                    
+                    Image(systemName: "lock")
+                    .resizable()
+                    .frame(width: 15, height: 18)
+                    .foregroundColor(.black)
+                    
+                    SecureField("Password", text: self.$pass)
+                    
+
+                    
+                }.padding(.vertical, 20)
+                
+            }
+            .padding(.vertical)
+            .padding(.horizontal, 20)
+            .padding(.bottom, 40)
+            .background(Color.white)
+            .cornerRadius(10)
+            .padding(.top, 25)
+            
+            
+            Button(action: {
+                
+            }) {
+                
+                Text("LOGIN")
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .padding(.vertical)
+                    .frame(width: UIScreen.main.bounds.width - 100)
+                
+            }.background(
+            
+                LinearGradient(gradient: .init(colors: [Color("Color2"),Color("Color1"),Color("Color")]), startPoint: .leading, endPoint: .trailing)
+            )
+            .cornerRadius(8)
+            .offset(y: -40)
+            .padding(.bottom, -40)
+            .shadow(radius: 15)
+        }
+    }
+}
+
+struct SignUp : View {
+    
+    @State var mail = ""
+    @State var pass = ""
+    @State var repass = ""
+    
+    var body : some View{
+        
+        VStack{
+            
+            VStack{
+                
+                HStack(spacing: 15){
+                    
+                    Image(systemName: "person")
+                        .foregroundColor(.black)
+                    
+                    TextField("Enter Username", text: self.$mail)
+                    
+                }.padding(.vertical, 20)
+                
+                Divider()
+                
+                HStack(spacing: 15){
+                    
+                    Image(systemName: "lock")
+                    .resizable()
+                    .frame(width: 15, height: 18)
+                    .foregroundColor(.black)
+                    
+                    SecureField("Password", text: self.$pass)
+ 
+                    
+                }.padding(.vertical, 20)
+                
+                Divider()
+                
+                HStack(spacing: 15){
+                    
+                    Image(systemName: "lock")
+                    .resizable()
+                    .frame(width: 15, height: 18)
+                    .foregroundColor(.black)
+                    
+                    SecureField("Re-Enter", text: self.$repass)
+                    
+
+                    
+                }.padding(.vertical, 20)
+                
+            }
+            .padding(.vertical)
+            .padding(.horizontal, 20)
+            .padding(.bottom, 40)
+            .background(Color.white)
+            .cornerRadius(10)
+            .padding(.top, 25)
+            
+            
+            Button(action: {
+                
+            }) {
+                
+                Text("SIGNUP")
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .padding(.vertical)
+                    .frame(width: UIScreen.main.bounds.width - 100)
+                
+            }.background(
+            
+                LinearGradient(gradient: .init(colors: [Color("Color2"),Color("Color1"),Color("Color")]), startPoint: .leading, endPoint: .trailing)
+            )
+            .cornerRadius(8)
+            .offset(y: -40)
+            .padding(.bottom, -40)
+            .shadow(radius: 15)
+        }
     }
 }
---
+++ Frontend/iOSApp/CyTalk/CyTalk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CyTalk
+//
+//  Created by Elijah Valine on 9/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

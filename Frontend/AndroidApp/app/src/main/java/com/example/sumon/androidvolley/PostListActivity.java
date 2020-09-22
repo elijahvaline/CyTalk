f36f14e49facbc4a842efb1d0bd9331ba309bf4b
cjwong <cjwong@iastate.edu>
2020-09-21 21:43:40 -0500

    List Post Activity

---
+++ Frontend/AndroidApp/app/src/main/java/com/example/sumon/androidvolley/PostListActivity.java
@@ -0,0 +1,13 @@
+package com.example.sumon.androidvolley.utils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_list);
+    }
+}

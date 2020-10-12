398e82c04193df7eab9244da083190c43692a405
cjwong <cjwong@iastate.edu>
2020-10-12 00:11:34 -0500

    Attempt user controller image functions

--- Backend/Springboot/src/main/java/myProject/UserController.java
+++ Backend/Springboot/src/main/java/myProject/UserController.java
@@ -1,19 +1,43 @@
 package myProject;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import uploading.StorageService;
 
 @RestController
 public class UserController {
 
 	@Autowired
 	UserDB db;
+	StorageService storage;
+	
+	ServletContext servletContext;
 
+	@Autowired
+	public UserController(StorageService storageService) {
+		storage = storageService;
+	}
 
 	@GetMapping("/user/{id}")
-	User getPerson(@PathVariable Integer id) {
+	User getPerson(@PathVariable Integer id, HttpServletResponse response) {
+		InputStream in = servletContext.getResourceAsStream(storage.load(db.findOne(id).getEmail()).toString());
+	    response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+
+	    try {
+			IOUtils.copy(in, response.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return db.findOne(id);
 	}
 	
--- Backend/Springboot/src/test/java/myProject/ImageServiceTest.java
+++ Backend/Springboot/src/test/java/myProject/ImageServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -46,7 +47,7 @@ public class ImageServiceTest {
 		service.store(new MockMultipartFile("test", "test.txt", MediaType.TEXT_PLAIN_VALUE,
 				"Hello World!".getBytes()));
 		assertThat(service.load("test.txt")).exists();
-		assertEquals("target/images/test.txt", service.load("test.txt").toString());
+		assertEquals("target" + File.separator + "images" + File.separator + "test.txt", service.load("test.txt").toString());
 	}
 
 	@Test

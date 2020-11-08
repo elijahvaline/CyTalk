ccfe22d3cfaf064e1024c3dad48a8f38e8a5f698
cjwong <cjwong@iastate.edu>
2020-11-08 15:28:47 -0600

    StorageService APIs

--- Backend/Springboot/src/main/java/myProject/uploading/StorageService.java
+++ Backend/Springboot/src/main/java/myProject/uploading/StorageService.java
@@ -3,13 +3,29 @@ package myProject.uploading;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.nio.file.Path;
-
+/**
+ * Service for uploading images/files
+ * @author Casey Wong
+ */
 public interface StorageService {
 
+	/**
+	 * Uploads the file
+	 * @param file the files to be uploaded
+	 */
 	void store(MultipartFile file);
 
+	/**
+	 * Loads the path to the named file
+	 * @param filename the string name of the file
+	 * @return the path to the named file on the server
+	 */
 	Path load(String filename);
 	
+	/**
+	 * Deletes the named file from storage
+	 * @param filename the string name of the file
+	 */
 	void delete(String filename);
 }
 
